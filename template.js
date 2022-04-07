58b0306e12d86cab349b5b368fcbd944919d542d
JP Wogaman II <jpwogaman@gmail.com>
2022-04-07 16:18:23 -0400

    changed some ifs to else ifs, messed around with beautify settings

--- template.js
+++ template.js
@@ -1,183 +1,184 @@
 module.exports = {
+		oscInFilter: function (data) {
 
-	oscInFilter: function(data) {
+			var { address, args, host, port } = data;
 
-		var {
-			address,
-			args,
-			host,
-			port
-		} = data
+			if (address === "/control" && args[1].value === 126 && args[2].value !== 0) {
 
-		if (address === '/control' && args[1].value === 126 && args[2].value !== 0) {
+				send("midi", "OSC4", "/control", 1, 127, 127);
 
-			send('midi', 'OSC4', '/control', 1, 127, 127)
+			} else {}
 
-		} else {
+			var tracks = loadJSON("../template/tracks.json");
 
-		}
+			if (address === "/key_pressure") {
 
-		var tracks = loadJSON('../template/tracks.json')
+				var x = args[1].value;
+				var y = args[2].value;
+				x = x * 128 + y;
 
-		if (address === '/key_pressure') {
+				receive("/text_1", tracks[x].Track);
+				tracks = [tracks[x]];
 
-			var x = args[1].value
-			var y = args[2].value
+				var artButtonsNamesVars = [];
+				var artButtonsModesVars = [];
+				var artButtonsTypesVars = [];
+				var artButtonsCodesVars = [];
+				var artButtonsColorsVars = [];
+				var artButtonsDefaultsVars = [];
+				var artButtonsOnsVars = [];
+				var artButtonsOffsVars = [];
+				var artButtonsInputsVars = [];
 
-			x = (x * 128) + y
+				var fadCodesVars = [];
+				var fadDefaultsVars = [];
+				var fadNamesVars = [];
 
-			receive('/text_1', tracks[x].Track)
-			tracks = [tracks[x]]
+				var artButtonsNames = [];
+				var artButtonsModes = [];
+				var artButtonsTypes = [];
+				var artButtonsCodes = [];
+				var artButtonsDefaults = [];
+				var artButtonsOns = [];
+				var artButtonsOffs = [];
+				var artButtonsInputs = [];
 
-			var artButtonsNamesVars = [];
-			var artButtonsModesVars = [];
-			var artButtonsTypesVars = [];
-			var artButtonsCodesVars = [];
-			var artButtonsColorsVars = [];
-			var artButtonsDefaultsVars = [];
-			var artButtonsOnsVars = [];
-			var artButtonsOffsVars = [];
-			var artButtonsInputsVars = [];
-			var fadCodesVars = [];
-			var fadDefaultsVars = [];
-			var fadNamesVars = [];
+				var fadCodes = [];
+				var fadDefaults = [];
+				var fadNames = [];
 
-			var artButtonsNames = [];
-			var artButtonsModes = [];
-			var artButtonsTypes = [];
-			var artButtonsCodes = [];
-			var artButtonsDefaults = [];
-			var artButtonsOns = [];
-			var artButtonsOffs = [];
-			var artButtonsInputs = [];
-			var fadCodes = [];
-			var fadDefaults = [];
-			var fadNames = [];
+				for (var key in tracks) {
 
-			for (var key in tracks) {
+					if (!tracks.hasOwnProperty(key)) continue;
+					var obj = tracks[key];
 
-				if (!tracks.hasOwnProperty(key)) continue;
-				var obj = tracks[key];
+					for (var prop in obj) {
 
-				for (var prop in obj) {
+						if (!obj.hasOwnProperty(prop)) continue;
 
-					if (!obj.hasOwnProperty(prop)) continue;
-					if (prop.includes('_Name')) {
-						artButtonsNames.push(obj[prop])
-					}
-					if (prop.includes('_Mode')) {
-						artButtonsModes.push(obj[prop])
-					}
-					if (prop.includes('_Type')) {
-						artButtonsTypes.push(obj[prop])
-					}
-					if (prop.includes('_Code')) {
-						artButtonsCodes.push(obj[prop])
-					}
-					if (prop.includes('_Default')) {
-						artButtonsDefaults.push(obj[prop])
-					}
-					if (prop.includes('_On')) {
-						artButtonsOns.push(obj[prop])
-					}
-					if (prop.includes('_Off')) {
-						artButtonsOffs.push(obj[prop])
-					}
-					if (prop.includes('FadA')) {
-						fadCodes.push(obj[prop])
-					}
-					if (prop.includes('FadB')) {
-						fadDefaults.push(obj[prop])
-					}
-					if (prop.includes('FadC')) {
-						fadNames.push(obj[prop])
-					}
-				}
-			}
+						if (prop.includes("_Name")) {
 
-			for (let i = 0; i < 9; i++) {
+							artButtonsNames.push(obj[prop]);
 
-				fadCodesVars[i] = '/CC' + (i + 1) + '_increment_value';
-				fadDefaultsVars[i] = '/CC_Preset_CC' + (i + 1) + '_Default';
-				fadNamesVars[i] = '/CC' + (i + 1) + '_display_Setting';
+						} else if (prop.includes("_Mode")) {
 
-				receive(fadCodesVars[i], parseInt(fadCodes[i]))
-				receive(fadDefaultsVars[i], parseInt(fadDefaults[i]))
-				receive(fadNamesVars[i], fadNames[i])
+							artButtonsModes.push(obj[prop]);
 
-				if (fadCodes[i] !== null) {
+						} else if (prop.includes("_Type")) {
 
-					send('midi', 'OSC4', '/control', 1, parseInt(fadCodes[i]), parseInt(fadDefaults[i]))
+							artButtonsTypes.push(obj[prop]);
 
-				} else {
-					continue
-				}
-			}
+						} else if (prop.includes("_Code")) {
 
+							artButtonsCodes.push(obj[prop]);
 
-			for (let i = 0; i < 18; i++) {
+						} else if (prop.includes("_Default")) {
 
-				artButtonsNamesVars[i] = '/art' + (i + 1) + 'name';
-				artButtonsModesVars[i] = '/art' + (i + 1) + 'mode';
-				artButtonsTypesVars[i] = '/art' + (i + 1) + 'type';
-				artButtonsCodesVars[i] = '/art' + (i + 1) + 'code';
-				artButtonsColorsVars[i] = '/art' + (i + 1) + 'color';
-				artButtonsDefaultsVars[i] = '/art' + (i + 1) + 'default';
-				artButtonsOnsVars[i] = '/art' + (i + 1) + 'on';
-				artButtonsOffsVars[i] = '/art' + (i + 1) + 'off';
-				artButtonsInputsVars[i] = '/art' + (i + 1) + 'input';
+							artButtonsDefaults.push(obj[prop]);
 
-				receive(artButtonsNamesVars[i], artButtonsNames[i])
-				receive(artButtonsModesVars[i], artButtonsModes[i])
-				receive(artButtonsTypesVars[i], String(artButtonsTypes[i]))
-				receive(artButtonsCodesVars[i], parseInt(artButtonsCodes[i]))
-				receive(artButtonsDefaultsVars[i], parseInt(artButtonsDefaults[i]))
-				receive(artButtonsOnsVars[i], parseInt(artButtonsOns[i]))
-				receive(artButtonsOffsVars[i], parseInt(artButtonsOffs[i]))
+						} else if (prop.includes("_On")) {
 
-				//NESTED IF'S PROBABLY NOT GREAT IDEA
+							artButtonsOns.push(obj[prop]);
 
-				if (artButtonsNames[i] === "" || artButtonsNames[i] === null) {
+						} else if (prop.includes("_Off")) {
 
-					receive(artButtonsInputsVars[i], 'true')
-					receive(artButtonsColorsVars[i], "#A9A9A9")
-					receive(artButtonsModesVars[i], 0.15) //alphaFillOff for buttons 1&2 alphaFillOn for rest
+							artButtonsOffs.push(obj[prop]);
 
-				} else {
+						} else if (prop.includes("FadA")) {
 
-					receive(artButtonsModesVars[i], 0.75)
-					if (artButtonsNamesVars[i] === '/art1name' || artButtonsNamesVars[i] === '/art2name') {
+							fadCodes.push(obj[prop]);
 
-						receive(artButtonsInputsVars[i], 'false')
-						receive(artButtonsColorsVars[i], "#a86739")
-						send('midi', 'OSC3', String(artButtonsTypes[i]), 1, parseInt(artButtonsCodes[i]), parseInt(artButtonsDefaults[i]))
+						} else if (prop.includes("FadB")) {
 
-					} else {
+							fadDefaults.push(obj[prop]);
 
-						receive(artButtonsColorsVars[i], "#6dfdbb")
-						receive(artButtonsDefaultsVars[i], parseInt(artButtonsDefaults[i]))
+						} else if (prop.includes("FadC")) {
 
-						if (parseInt(artButtonsDefaults[i]) !== 0) {
-							send('midi', 'OSC4', String(artButtonsTypes[i]), 1, parseInt(artButtonsCodes[i]), parseInt(artButtonsDefaults[i]))
+							fadNames.push(obj[prop]);
 
 						} else {
 
-							receive(artButtonsModesVars[i], 0.15)
+							continue
 
 						}
 					}
 				}
+				for (let i = 0; i < 9; i++) {
+
+					fadCodesVars[i] = "/CC" + (i + 1) + "_increment_value";
+					fadDefaultsVars[i] = "/CC_Preset_CC" + (i + 1) + "_Default";
+					fadNamesVars[i] = "/CC" + (i + 1) + "_display_Setting";
+
+					receive(fadCodesVars[i], parseInt(fadCodes[i]));
+					receive(fadDefaultsVars[i], parseInt(fadDefaults[i]));
+					receive(fadNamesVars[i], fadNames[i]);
+
+					if (fadCodes[i] !== null) {
+
+						send("midi", "OSC4", "/control", 1, parseInt(fadCodes[i]), parseInt(fadDefaults[i]));
+
+					} else {
+
+						continue;
+
+					}
+				}
+				for (let i = 0; i < 18; i++) {
+
+					artButtonsNamesVars[i] = "/art" + (i + 1) + "name";
+					artButtonsModesVars[i] = "/art" + (i + 1) + "mode";
+					artButtonsTypesVars[i] = "/art" + (i + 1) + "type";
+					artButtonsCodesVars[i] = "/art" + (i + 1) + "code";
+					artButtonsColorsVars[i] = "/art" + (i + 1) + "color";
+					artButtonsDefaultsVars[i] = "/art" + (i + 1) + "default";
+					artButtonsOnsVars[i] = "/art" + (i + 1) + "on";
+					artButtonsOffsVars[i] = "/art" + (i + 1) + "off";
+					artButtonsInputsVars[i] = "/art" + (i + 1) + "input";
+
+					receive(artButtonsNamesVars[i], artButtonsNames[i]);
+					receive(artButtonsModesVars[i], artButtonsModes[i]);
+					receive(artButtonsTypesVars[i], String(artButtonsTypes[i]));
+					receive(artButtonsCodesVars[i], parseInt(artButtonsCodes[i]));
+					receive(artButtonsDefaultsVars[i], parseInt(artButtonsDefaults[i]);
+					receive(artButtonsOnsVars[i], parseInt(artButtonsOns[i]));
+					receive(artButtonsOffsVars[i], parseInt(artButtonsOffs[i]));
+
+						//NESTED IF'S PROBABLY NOT GREAT IDEA
+
+						if (artButtonsNames[i] === "" || artButtonsNames[i] === null) {
+
+							receive(artButtonsInputsVars[i], "true");
+							receive(artButtonsColorsVars[i], "#A9A9A9");
+							receive(artButtonsModesVars[i], 0.15); //alphaFillOff for buttons 1&2 alphaFillOn for rest
+
+						} else {
+
+							receive(artButtonsModesVars[i], 0.75);
+
+							if (artButtonsNamesVars[i] === "/art1name" || artButtonsNamesVars[i] === "/art2name") {
+
+								receive(artButtonsInputsVars[i], "false");
+								receive(artButtonsColorsVars[i], "#a86739");
+								send("midi", "OSC3", String(artButtonsTypes[i]), 1, parseInt(artButtonsCodes[i]), parseInt(artButtonsDefaults[i]));
+
+							} else {
+
+								receive(artButtonsColorsVars[i], "#6dfdbb");
+								receive(artButtonsDefaultsVars[i], parseInt(artButtonsDefaults[i]));
+
+								if (parseInt(artButtonsDefaults[i]) !== 0) {
+
+									send("midi", "OSC4", String(artButtonsTypes[i]), 1, parseInt(artButtonsCodes[i]), parseInt(artButtonsDefaults[i]));
+
+								} else {
+
+									receive(artButtonsModesVars[i], 0.15);
+
+								}
+							}
+						}
+					}
+				}
+				return { address, args, host, port };
 			}
-
-		}
-
-		return {
-			address,
-			args,
-			host,
-			port
-		}
-
-	},
-
-}
+		};
